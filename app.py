e4838d3c3944ac16adea96149b1be2e7e7d0c01f
Magnus Rosén <mangez84@gmail.com>
2021-07-28 09:11:02 +0000

    Add routes and views to the other pages

--- app.py
+++ app.py
@@ -26,6 +26,21 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
