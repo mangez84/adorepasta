e669e55f10e9ed2c418bf0395aaef200d858cc84
Magnus Rosén <mangez84@gmail.com>
2021-08-02 21:07:42 +0000

    Rewrite JavaScript functions to get consistent numbering in the ingredient fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,59 @@
-/*
-Function to add ingredients and set proper attribute values
+/* 
+The attributes for, id and name should be unique
 */
 
-$(".add-ingredient").click(function (event) {
+function modifyAttributes() {
+    let numIngredients = 0;
+    $(".recipe-ingredient label").each(function (index) {
+        numIngredients = index + 1;
+        $(this).html("Ingredient " + numIngredients).attr("for", "ingredient-" + numIngredients);
+    });
+    $(".recipe-ingredient input").each(function (index) {
+        numIngredients = index + 1;
+        $(this).attr("id", "ingredient-" + numIngredients).attr("name", "ingredient-" + numIngredients).attr(
+            "placeholder", "Ingredient " + numIngredients);
+    });
+    $(".recipe-quantity label").each(function (index) {
+        numIngredients = index + 1;
+        $(this).attr("for", "quantity-" + numIngredients);
+    });
+    $(".recipe-quantity input").each(function (index) {
+        numIngredients = index + 1;
+        $(this).attr("id", "quantity-" + numIngredients).attr("name", "quantity-" + numIngredients);
+    });
+    $(".recipe-unit label").each(function (index) {
+        numIngredients = index + 1;
+        $(this).attr("for", "unit-" + numIngredients);
+    });
+    $(".recipe-unit input").each(function (index) {
+        numIngredients = index + 1;
+        $(this).attr("id", "unit-" + numIngredients).attr("name", "unit-" + numIngredients);
+    });
+}
+
+/*
+Function to add ingredients
+*/
+
+$(".add-ingredient > button").click(function (event) {
     event.preventDefault();
     let ingredientDetails = $(".ingredient-details").children().first().clone();
-    let numIngredients = $(".ingredient-details").children().length + 1;
-    // Attributes for, id and name should be unique 
-    ingredientDetails.find(".recipe-ingredient label").html("Ingredient " + numIngredients).attr("for", "ingredient-" + numIngredients);
-    ingredientDetails.find(".recipe-ingredient input").val("").attr("id", "ingredient-" + numIngredients).attr(
-        "name", "ingredient-" + numIngredients).attr("placeholder", "Ingredient " + numIngredients);
-    ingredientDetails.find(".recipe-quantity label").attr("for", "quantity-" + numIngredients);
-    ingredientDetails.find(".recipe-quantity input").val("").attr("id", "quantity-" + numIngredients).attr(
-        "name", "quantity-" + numIngredients);
-    ingredientDetails.find(".recipe-unit label").attr("for", "unit-" + numIngredients);
-    ingredientDetails.find(".recipe-unit input").val("").attr("id", "unit-" + numIngredients).attr(
-        "name", "unit-" + numIngredients);
-    // Append cloned and modified elements
+    ingredientDetails.find(".recipe-ingredient input").val("");
+    ingredientDetails.find(".recipe-quantity input").val("");
+    ingredientDetails.find(".recipe-unit input").val("");
     $(".ingredient-details").append(ingredientDetails);
-})
+    modifyAttributes();
+});
+
+/*
+Function to remove ingredients
+*/
+
+$(document).on("click", ".remove-ingredient > button", function (event) {
+    event.preventDefault();
+    let numIngredients = $(".ingredient-details").children().length;
+    if (numIngredients !== 1) {
+        $(this).parent().parent().remove();
+    }
+    modifyAttributes();
+});
